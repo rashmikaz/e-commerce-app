e1035955e36fd7cf597b66eceb66d6b87b65912c
rashmikaz <phrashmika@gmail.com>
2025-04-05 14:34:39 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,26 +7,62 @@ import AdminDashboard from "./pages/admin-view/dashboard";
 import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
 import AdminFeatures from "./pages/admin-view/features";
+import CheckAuth from "./components/common/check-auth";
 
 
 function App() {
+
+  const isAuthenticated = false;
+  const  user = null
  
 
   return (
  <div className="flex flex-col overflow-hidden bg-white">
   <h1>header components</h1>
   <Routes>
-    <Route path="/auth" element={<AuthLayout/>}>
+    <Route path="/auth" element={
+      <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+        <AuthLayout/>
+      </CheckAuth>
+      }>
+      
     <Route path="login" element={<AuthLogin/>}/>
     <Route path="register" element={<AuthRegister/>}/>
     </Route>
+
   
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="dashboard" element={<AdminDashboard />} />
-          <Route path="products" element={<AdminProducts />} />
-          <Route path="orders" element={<AdminOrders />} />
-          <Route path="features" element={<AdminFeatures />} />
+    <Route path="/admin" element={
+        <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+          <AdminLayout />
+        </CheckAuth>
+          }>
+
+        <Route path="dashboard" element={<AdminDashboard />} />
+        <Route path="products" element={<AdminProducts />} />
+        <Route path="orders" element={<AdminOrders />} />
+        <Route path="features" element={<AdminFeatures />} />
+
+    </Route>
+
+    <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="account" element={<ShoppingAccount />} />
+          <Route path="paypal-return" element={<PaypalReturnPage />} />
+          <Route path="payment-success" element={<PaymentSuccessPage />} />
+          <Route path="search" element={<SearchProducts />} />
         </Route>
+        <Route path="/unauth-page" element={<UnauthPage />} />
+        <Route path="*" element={<NotFound />} />
+        
   </Routes>
       
 
