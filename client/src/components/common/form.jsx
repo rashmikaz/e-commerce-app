62e247d792d469898436d17ef5f6e03f9bbd0436
rashmikaz <phrashmika@gmail.com>
2025-04-06 01:24:34 +0530

    button

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,6 +1,104 @@
 
 
-function CommonForm() {
+function CommonForm( 
+    formControls,
+    formData,
+    setFormData,
+    onSubmit,
+    buttonText,
+    isBtnDisabled) {
+
+
+    function renderInputsByComponentType(getControlItem) {
+        let element = null;
+        const value = formData[getControlItem.name] || "";
+    
+        switch (getControlItem.componentType) {
+          case "input":
+            element = (
+              <Input
+                name={getControlItem.name}
+                placeholder={getControlItem.placeholder}
+                id={getControlItem.name}
+                type={getControlItem.type}
+                value={value}
+                onChange={(event) =>
+                  setFormData({
+                    ...formData,
+                    [getControlItem.name]: event.target.value,
+                  })
+                }
+              />
+            );
+    
+            break;
+          case "select":
+            element = (
+              <Select
+                onValueChange={(value) =>
+                  setFormData({
+                    ...formData,
+                    [getControlItem.name]: value,
+                  })
+                }
+                value={value}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder={getControlItem.label} />
+                </SelectTrigger>
+                <SelectContent>
+                  {getControlItem.options && getControlItem.options.length > 0
+                    ? getControlItem.options.map((optionItem) => (
+                        <SelectItem key={optionItem.id} value={optionItem.id}>
+                          {optionItem.label}
+                        </SelectItem>
+                      ))
+                    : null}
+                </SelectContent>
+              </Select>
+            );
+    
+            break;
+          case "textarea":
+            element = (
+              <Textarea
+                name={getControlItem.name}
+                placeholder={getControlItem.placeholder}
+                id={getControlItem.id}
+                value={value}
+                onChange={(event) =>
+                  setFormData({
+                    ...formData,
+                    [getControlItem.name]: event.target.value,
+                  })
+                }
+              />
+            );
+    
+            break;
+    
+          default:
+            element = (
+              <Input
+                name={getControlItem.name}
+                placeholder={getControlItem.placeholder}
+                id={getControlItem.name}
+                type={getControlItem.type}
+                value={value}
+                onChange={(event) =>
+                  setFormData({
+                    ...formData,
+                    [getControlItem.name]: event.target.value,
+                  })
+                }
+              />
+            );
+            break;
+        }
+    
+        return element;
+      }
+    
 
     return (
         <form onSubmit={onSubmit}>
