fcbe4cb6b54350c88882c7c333f209e9f3f0118d
rashmikaz <phrashmika@gmail.com>
2025-04-06 15:39:34 +0530

    navbar

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 const Login = () => {
   return (
-    <div>
-      login
-    </div>
+    <>
+    <Navbar />
+    </>
   )
 }
 
