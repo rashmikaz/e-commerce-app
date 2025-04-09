b8e5925abeedf65a3e1cef50d9d4517fcab46f5d
rashmikaz <phrashmika@gmail.com>
2025-04-09 15:26:36 +0530

    set use state

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import { Link } from 'react-router-dom'
 import PasswordInput from '../../components/input/PasswordInput'
 
 const Login = () => {
+
+    const[email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+    const[error,setError] = useState(null);
+
   return (
     <>
     <Navbar />
 
     <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-            <form onSubmit={()=>{}}>
+            <form onSubmit={}>
                 <h4 className="text-2xl mb-7">Login</h4>
 
-                <input type="text" placeholder="Email" className="input-box" />
-                <PasswordInput />
+                <input 
+                type="text" 
+                placeholder="Email" 
+                className="input-box"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)} />
+
+                <PasswordInput 
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                />
 
                 <button type="submit" className="btn-primary">
                     Login
