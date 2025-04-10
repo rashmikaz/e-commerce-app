6443bff1eb62e5b321aee1f89adc33a80717ea19
rashmikaz <phrashmika@gmail.com>
2025-04-10 13:40:26 +0530

    navigate

--- client/src/components/Cards/ProfileInfo.js
+++ client/src/components/Cards/ProfileInfo.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { getInitials } from '../../utills/helper'
 
 const ProfileInfo = ({onLogout}) => {
   return (
     <div className="flex items-center gap-3">
-        <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100 ">TU</div>
+        <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100 ">{getInitials("Jhon Wick")}</div>
             <div>
                 <p className="text-sm font-medium">Jhon</p>
                 <button className="" onClick={onLogout}>
