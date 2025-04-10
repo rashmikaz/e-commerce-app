df459efd25ec0a0f30c685da0d919da1392f17f7
rashmikaz <phrashmika@gmail.com>
2025-04-11 00:53:07 +0530

    note card

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
 
 const Home = () => {
   return (
     <>
     <Navbar />
+    <div className="container mx-auto">
+
+      <NoteCard />
+
+    </div>
       
     </>
   )
